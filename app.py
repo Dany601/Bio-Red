a4ab1ea066e509a90286d35b3733e8006e258d58
Daniela <danielaparra1104@gmail.com>
2026-04-16 21:54:33 -0500

    se realiza pagina inicio, dataset y analisis

--- app.py
+++ app.py
@@ -1,14 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "holi, Word!"
+# Ruta principal
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# Otras rutas del menú (puedes completarlas luego)
+@app.route('/proyecto')
+def proyecto():
+     return render_template('proyecto.html')
 
 
-@app.route("/Bio-Red/usuario/<nombre>/<apellido>")
-def mostrar_usuario(nombre, apellido):
-    return "Hola, {} {} ¿como estas?".format(nombre, apellido)
+@app.route('/dataset')
+def dataset():
+    return render_template('dataset.html')
 
-if __name__ == "__main__":
-    app.run(debug=True) 
+@app.route('/analisis')
+def analisis():
+    return render_template('analisis.html')
+
+@app.route('/kpis')
+def kpis():
+    return render_template('kpis.html')
+
+@app.route('/modelo')
+def modelo():
+    return render_template('modelo.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
